cd2d632e918bb0040dc38a7653c7592736d5ccac
Ryan Sampaio Filho <ryanfilhoryanfilho@gmail.com>
2015-10-01 16:55:59 -0300

    Modificações no cadastro de livros
    Remoção de inputs, criação de uma metodo para validar os dados da
    google, porém sem conlcusão.

--- app/controllers/cadastroLivroController.js
+++ app/controllers/cadastroLivroController.js
@@ -1,11 +1,16 @@
 'use strict';
 app.controller('cadastroLivroController', ['$scope', 'googleBookService', function ($scope, googleBookService) {
-	
-
+	$scope.livro = {
+		imagem: 'content/imagens/defaultbook.png',
+		isbn: [null, null],
+		titulo: null,
+		autores: null,
+		publicacao: null
+	};
 	$scope.pegarLivro = function (isbn) {
 		googleBookService.getBook(isbn, function (livroBack) {
 			$scope.livro = livroBack;
-			console.log(livroBack);
+			console.log(livroBack); // Testes
 		});
 	}
 
--- app/services/googleBookService.js
+++ app/services/googleBookService.js
@@ -1,24 +1,30 @@
 app.factory('googleBookService', ['$http', function ($http) {
+	googleBookServiceFactory = {};
+	var _getBook = function (valor, successCallBack) {
+		var url = "https://www.googleapis.com/books/v1/volumes?q=isbn:";				
 
-	return {
-		getBook: function (valor, successcb) {
-			var url = "https://www.googleapis.com/books/v1/volumes?q=isbn:";				
-
-			$http({method: 'GET', url:  (url + valor) }).
-				success(function (data, status, headers, config) {
-					var temp = data.items[0].volumeInfo;
-					info = {
-						isbn: [temp.industryIdentifiers[0], temp.industryIdentifiers[1]],
-						titulo: temp.title,
-						imagem : temp.imageLinks.thumbnail,					
-						autores: temp.authors.join(","),
-						paginas: temp.pageCount,
-						publicacao: temp.publishedDate,
-					};
-					successcb(info);
-			});
-		}
+		$http({method: 'GET', url:  (url + valor) })
+			.success(function (data, status, headers, config) {
+				var dadosLivro = _validador(data);
+				successCallBack(dadosLivro);
+		});
 	}
 
+	var _validador = function (data) {
+		temp = {};
+		dadosLivro = {};
+		typeof data.items[0].volumeInfo != 'undefined' ? temp = data.items[0].volumeInfo : temp = null;
+		typeof temp.industryIdentifiers != 'undefined'? dadosLivro.isbn = [temp.industryIdentifiers[0].identifier, temp.industryIdentifiers[1].identifier] : dadosLivro.isbn = null;
+		typeof temp.title != 'undefined' ? dadosLivro.titulo = temp.title : dadosLivro.titulo = null;
+		typeof temp.imageLinks.thumbnail != 'undefined'? dadosLivro.imagem = temp.imageLinks.thumbnail : dadosLivro.imagem = null;
+		typeof temp.authors != 'undefined'? dadosLivro.autores = temp.authors.join(",") : dadosLivro.autores = null;
+		typeof temp.pageCount != 'undefined'? dadosLivro.paginas = temp.pageCount : dadosLivro.paginas = null;
+		typeof temp.publishedDate != 'undefined'? dadosLivro.publicacao = temp.publishedDate : dadosLivro.publicacao = null;
+		return dadosLivro;
+	}
+
+	googleBookServiceFactory.getBook = _getBook;
+
+	return googleBookServiceFactory;
 
 }]);
