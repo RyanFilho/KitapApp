d80c2c6dd53058b83a293a36a106340efcab61fa
Ryan Sampaio Filho <ryanfilhoryanfilho@gmail.com>
2015-10-02 16:43:16 -0300

    Criação do Serviço de Cadastro de Livro
    Para enviar para Web Api

---
+++ app/services/cadastroLivroService.js
@@ -0,0 +1,7 @@
+app.factory('cadastroLivroService', ['$http', function ($http) {
+	var _postLivro = function () {	
+			// $http.post();
+	}
+	cadastroLivroServiceFactory = _postLivro;
+	return cadastroLivroServiceFactory;
+}]);
